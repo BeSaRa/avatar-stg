848c5e6efe1a1c3a4a3daa51352a5c31f027c480
Moaed Alloula <moaed.alloula@invi-aligners.com>
2024-12-02 15:09:41 +0300

    fix:hide video indexer

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -4,10 +4,6 @@ import { LandingComponent } from '@/views/landing/landing.component'
 const routes: Routes = [
   { path: '', component: LandingComponent },
   {
-    path: 'video-indexer',
-    loadComponent: () => import('@/views/video-indexer/video-indexer.component').then(c => c.VideoIndexerComponent),
-  },
-  {
     path: 'search',
     loadComponent: () => import('@/views/ai-search/ai-search.component').then(c => c.AiSearchComponent),
   },
