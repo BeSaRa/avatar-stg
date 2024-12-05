2b2da51a1d917b0ef521a8dfeffd995e77c25520
Moaed Alloula <moaed.alloula@invi-aligners.com>
2024-12-05 10:16:14 +0300

    define globalthis

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/no-require-imports */
 ;(window as any).global = window
 global.process = require('process')
+if (typeof globalThis === 'undefined') {
+  ;(window as any).globalThis = window
+}
