a283f80c98b7fdeeaa3e55ab5fbeb4e8cb3bc2ba
Moaed Alloula <moaed.alloula@invi-aligners.com>
2024-12-17 17:34:23 +0300

    feat: choose the bot to interact with in avatar and chat popup

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,7 +1,10 @@
 export const EndPoints = {
   BASE_URL: '',
   SPEECH_TOKEN: 'speech/token',
-  CHAT: 'chatbot/chat/website',
+  CHAT: 'chatbot/chat',
+  INTERACTIVE: 'interactive',
+  INTERACTIVE_CHAT: 'chatbot/chat/interactive',
+  INTERACTIVE_ACTION: 'interactive/action',
   AVATAR: 'avatar',
   LOCALS: 'http://localhost:3333',
   VIDEO_INDEXER: 'https://www.videoindexer.ai/embed',
