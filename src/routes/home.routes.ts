74035834dfe9d1a0d7d47ee4dbc67d5440854cb7
Mamoun-Hamed <en15hamed1992@gmail.com>
2025-02-04 14:35:57 +0300

    Add authentication

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -1,20 +1,28 @@
-import { Routes } from '@angular/router'
+import { PermissionRouteData } from '@/contracts/permission-rout-data'
+import { PermissionGuard } from '@/guards/permission.guard'
 import { LandingComponent } from '@/views/landing/landing.component'
+import { Routes } from '@angular/router'
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   {
     path: 'search',
     loadComponent: () => import('@/views/ai-search/ai-search.component').then(c => c.AiSearchComponent),
+    canActivate: [PermissionGuard.canActivate],
+    data: { permissions: ['SEARCH'], hasAnyPermission: false } as PermissionRouteData,
   },
   {
     path: 'chat-history',
     loadComponent: () => import('@/views/chat-history/chat-history.component').then(c => c.ChatHistoryComponent),
+    canActivate: [PermissionGuard.canActivate],
+    data: { permissions: ['CHATBOT'], hasAnyPermission: false } as PermissionRouteData,
   },
   {
     path: 'web-crawler',
     loadComponent: () =>
       import('@/views/web-crawler-report/web-crawler-report.component').then(c => c.WebCrawlerReportComponent),
+    canActivate: [PermissionGuard.canActivate],
+    data: { permissions: ['SEARCH'], hasAnyPermission: false } as PermissionRouteData,
   },
   {
     path: 'statistics',
@@ -23,6 +31,8 @@ const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('@/routes/admin.routes'),
+    canActivate: [PermissionGuard.canActivate],
+    data: { permissions: ['ADMIN'], hasAnyPermission: true } as PermissionRouteData,
   },
   {
     path: '**',
