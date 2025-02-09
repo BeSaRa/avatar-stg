e4f746c4e93ad84193772ded61ee2bb2b57090ee
Moaed Alloula <moaed.alloula@invi-aligners.com>
2025-02-09 11:27:24 +0300

    refactor: refactor env urls

--- src/http-interceptors/api-key.interceptor.ts
+++ src/http-interceptors/api-key.interceptor.ts
@@ -5,15 +5,19 @@ import { inject } from '@angular/core'
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
   const configService = inject(ConfigService)
 
+  const environmentKunaKeys: Record<string, string> = {
+    AQARAT_DEV: configService.CONFIG.KUNA_DEV,
+    AQARAT_STG: configService.CONFIG.KUNA_STG,
+    AQARAT_PROD: configService.CONFIG.KUNA_PROD,
+  }
+
+  // Get the KUNA key based on the current environment
+  const currentEnvironment = configService.CONFIG.BASE_ENVIRONMENT
+  const apiKey = environmentKunaKeys[currentEnvironment]
+
   return next(
     req.clone({
-      setHeaders: {
-        ...(configService.CONFIG.BASE_ENVIRONMENT === 'AQARAT_STG'
-          ? {
-              'x-functions-key': configService.CONFIG.KUNA,
-            }
-          : undefined),
-      },
+      setHeaders: apiKey ? { 'x-functions-key': apiKey } : {},
     })
   )
 }
