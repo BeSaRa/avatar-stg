3ecfa4759178d2923f6abc1bc7cc647912130e9e
Moaed Alloula <moaed.alloula@invi-aligners.com>
2025-02-24 14:59:45 +0300

    refactor: hide items from header and webcrawler

--- src/resources/menu-items.ts
+++ src/resources/menu-items.ts
@@ -30,32 +30,11 @@ export const MENU_ITEMS: MenuItem[] = [
     permissions: ['AVATAR'],
   },
   {
-    id: 5,
-    label: 'video_indexer',
-    route: '/home/video-indexer',
-    svg: 'VIDEO',
-    permissions: ['MEDIA'],
-  },
-  {
     id: 6,
     label: 'chat_history',
     route: '/home/chat-history',
     svg: 'HISTORY',
     permissions: ['CHATBOT'],
-  },
-  {
-    id: 7,
-    label: 'doc_analyzer',
-    route: '/home/doc-intelligence',
-    svg: 'DOCUMENT',
-    permissions: ['DOCUMENT_INTELLIGENCE'],
-  },
-  {
-    id: 8,
-    label: 'interactive_chatbot',
-    route: '/home/interactive-chatbot',
-    svg: 'BOT_ARM',
-    permissions: ['SPEECH'],
   },
   {
     id: 9,
