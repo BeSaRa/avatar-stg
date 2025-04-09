BeSaRa/avatar

feat: agent task managment chat

src/resources/menu-items.ts
@@ -38,55 +38,62 @@ export const MENU_ITEMS: MenuItem[] = [
   },
   {
     id: 6,
+    label: 'tasks_agent',
+    route: '/home/tasks-agent',
+    svg: 'BOT_ARM_FILL',
+    permissions: ['SPEECH'],
+  },
+  {
+    id: 7,
     label: 'web_crawler',
     route: '/home/web-crawler',
     svg: 'WEB_CRAWLER',
     permissions: ['SEARCH'],
   },
   {
-    id: 7,
+    id: 8,
     label: 'statistics',
     route: '/home/statistics',
     svg: 'POLL',
     permissions: ['ADMIN'],
   },
   {
-    id: 8,
+    id: 9,
     label: 'video_analyzer',
     route: '/home/video_analyzer',
     svg: 'VIDEO_ANALYSIS',
     permissions: [],
   },
   {
-    id: 9,
+    id: 10,
     label: 'doc_analyzer',
     route: '/home/doc-intelligence',
     svg: 'DOCUMENT',
     permissions: ['DOCUMENT_INTELLIGENCE'],
   },
   {
-    id: 10,
+    id: 11,
     label: 'avatar_chat',
     route: '/avatar',
     svg: 'AVATAR',
     permissions: ['AVATAR'],
   },
   {
-    id: 11,
+    id: 12,
     label: 'ms_avatar',
     route: '/ms-avatar',
     svg: 'MS_AVATAR',
     permissions: ['AVATAR'],
   },
   {
-    id: 12,
+    id: 13,
     label: 'video_indexer',
     route: '/home/video-indexer',
     svg: 'VIDEO',
     permissions: ['MEDIA'],
   },
   {
-    id: 13,
+    id: 14,
     label: 'video_generator',
     route: '/video-generator',
     svg: 'VIDEO_GENERATOR',

BeSaRa/avatar-stg

src/resources/menu-items.ts
@@ -37,28 +37,49 @@ export const MENU_ITEMS: MenuItem[] = [
     permissions: ['CHATBOT'],
   },
   {
-    id: 9,
+    id: 7,
     label: 'web_crawler',
     route: '/home/web-crawler',
     svg: 'WEB_CRAWLER',
     permissions: ['SEARCH'],
   },
   {
+    id: 8,
+    label: 'statistics',
+    route: '/home/statistics',
+    svg: 'POLL',
+    permissions: ['ADMIN'],
+  },
+  {
+    id: 10,
+    label: 'doc_analyzer',
+    route: '/home/doc-intelligence',
+    svg: 'DOCUMENT',
+    permissions: ['DOCUMENT_INTELLIGENCE'],
+  },
+  {
     id: 11,
+    label: 'avatar_chat',
+    route: '/avatar',
+    svg: 'AVATAR',
+    permissions: ['AVATAR'],
+  },
+  {
+    id: 12,
     label: 'ms_avatar',
     route: '/ms-avatar',
     svg: 'MS_AVATAR',
     permissions: ['AVATAR'],
   },
   {
-    id: 12,
+    id: 13,
     label: 'video_indexer',
     route: '/home/video-indexer',
     svg: 'VIDEO',
     permissions: ['MEDIA'],
   },
   {
-    id: 13,
+    id: 14,
     label: 'video_generator',
     route: '/video-generator',
     svg: 'VIDEO_GENERATOR',