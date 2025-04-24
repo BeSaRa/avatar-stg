ba625fb04465e6d56ba86558feae4b2b8e95fe1e
Moaed Alloula <moaed.alloula@invi-aligners.com>
2025-04-24 14:10:32 +0300

    fix: #65373 #65404 #65372 #65390 #65382

--- src/http-interceptors/api-key.interceptor.ts
+++ src/http-interceptors/api-key.interceptor.ts
@@ -1,13 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http'
 import { ConfigService } from '@/services/config.service'
 import { inject } from '@angular/core'
-import { finalize } from 'rxjs'
-import { MessageService } from '@/services/message.service'
-import { SHOW_SNACKBAR } from '@/http-contexts/show-snackbar'
 
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
   const configService = inject(ConfigService)
-  const messageService = inject(MessageService)
 
   const environmentKunaKeys: Record<string, string> = {
     AQARAT_DEV: configService.CONFIG.KUNA_DEV,
@@ -22,12 +18,6 @@ export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
   return next(
     req.clone({
       setHeaders: apiKey ? { 'x-functions-key': apiKey } : {},
-    })
-  ).pipe(
-    finalize(() => {
-      if (req.context.has(SHOW_SNACKBAR)) {
-        messageService.showInfo('Done successfully !')
-      }
     })
   )
 }
