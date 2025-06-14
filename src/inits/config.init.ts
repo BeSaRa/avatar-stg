01e0ce47ca79f0e8fb6ee116fd4b1a8ecf7fc602
BeSaRa <ahmedbesara@gmailcom>
2025-06-14 11:00:18 +0300

    fix: stop loading the app if the exception happened while asking for access token during APP_INITIALIZER

--- src/inits/config.init.ts
+++ src/inits/config.init.ts
@@ -1,7 +1,7 @@
 import { APP_INITIALIZER, Provider } from '@angular/core'
 import { ConfigService } from '@/services/config.service'
 import { UrlService } from '@/services/url.service'
-import { forkJoin, of, switchMap, tap } from 'rxjs'
+import { catchError, forkJoin, of, switchMap, tap } from 'rxjs'
 import { SpeechService } from '@/services/speech.service'
 import { LocalService } from '@/services/local.service'
 import { AuthService } from '@/services/auth.service'
@@ -22,7 +22,9 @@ export default {
         tap(() => urlService.setConfigService(configService)),
         tap(() => urlService.prepareUrls()),
         switchMap(() => local.load()),
-        switchMap(() => (tokenService.hasRefreshToken() ? auth.refreshToken() : of(null)))
+        switchMap(() =>
+          tokenService.hasRefreshToken() ? auth.refreshToken().pipe(catchError(() => of(null))) : of(null)
+        )
         // tap(() => injector.get(ApplicationUserService).checkAutoLogoutOnRefresh())
       )
   },
