7674f83b8b8ca3c2aea0f6c201050c135d304690
BeSaRa <ahmedbesara@gmailcom>
2025-06-15 03:24:48 +0300

    fix: user cannot click on the conversation from chat history

--- src/directives/disable-if-no-permission.directive.ts
+++ src/directives/disable-if-no-permission.directive.ts
@@ -1,13 +1,13 @@
-import { ApplicationUserService } from '@/views/auth/services/application-user.service'
 import { Directive, effect, ElementRef, HostListener, inject, input, Renderer2 } from '@angular/core'
 import { ALL_PERMISSIONS } from '../resources/all-permissions'
+import { EmployeeService } from '@/services/employee.service'
 
 @Directive({
   selector: '[appDisableIfNoPermission]',
   standalone: true,
 })
 export class DisableIfNoPermissionDirective {
-  private readonly appicationUser = inject(ApplicationUserService).$applicationUser()
+  private readonly employeeService = inject(EmployeeService)
   private readonly el = inject(ElementRef)
   private readonly renderer = inject(Renderer2)
 
@@ -16,7 +16,7 @@ export class DisableIfNoPermissionDirective {
   private hasPermission = true
 
   showEffect = effect(() => {
-    this.hasPermission = this.appicationUser.hasAllPermission(this.appDisableIfNoPermission()!)
+    this.hasPermission = this.employeeService.hasAllPermission(this.appDisableIfNoPermission()!)
     this.renderer.setProperty(this.el.nativeElement, 'disabled', !this.hasPermission)
     this.renderer.setAttribute(this.el.nativeElement, 'aria-disabled', String(!this.hasPermission))
     this.renderer.setStyle(this.el.nativeElement, 'pointer-events', this.hasPermission ? 'auto' : 'none')
